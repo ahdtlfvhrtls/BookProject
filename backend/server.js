4673af5398cdcb40421b8365a09a274706d79033
신현식 <hunsik777@gmail.com>
2026-04-02 17:57:24 +0900

    3

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mysql from "mysql2";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("DB 연결 실패:", err);
+  } else {
+    console.log("DB 연결 성공");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
