8efd5beae15151973cec1fb1c818bd091bbc660b
신현식 <hunsik777@gmail.com>
2026-04-02 22:14:08 +0900

    서버와 연결

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 import express from "express";
 import mysql from "mysql2";
 import axios from "axios";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const app = express();
 app.use(express.json());
