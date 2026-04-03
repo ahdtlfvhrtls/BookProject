042df8167c5f1f92523a8eed11ad2f24a3f4789b
신현식 <hunsik777@gmail.com>
2026-04-03 23:45:21 +0900

    css js 수정

--- backend/Pront/app.js
+++ backend/Pront/app.js
@@ -1,47 +1,38 @@
-const API = "http://localhost:3000/api";
-
-// 책 생성
 async function createBook() {
   const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
   const content = document.getElementById("content").value;
 
-  await fetch(`${API}/books`, {
+  await fetch("/api/books", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ title, content }),
+    body: JSON.stringify({ title, author, content }),
   });
 
-  alert("책 생성 완료!");
+  loadBooks();
 }
 
-// 목록 이동
-function goList() {
-  location.href = "/list.html";
+async function loadBooks() {
+  const res = await fetch("/api/books");
+  const books = await res.json();
+
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+
+  books.forEach((b) => {
+    const div = document.createElement("div");
+    div.className = "book";
+    div.innerHTML = `<b>${b.title}</b> - ${b.author}`;
+    div.onclick = () => showDetail(b.id);
+    list.appendChild(div);
+  });
 }
 
-// 목록 불러오기
-if (location.pathname.includes("list.html")) {
-  fetch(`${API}/books`)
-    .then((res) => res.json())
-    .then((data) => {
-      const list = document.getElementById("list");
-      data.forEach((book) => {
-        const div = document.createElement("div");
-        div.innerHTML = `<a href="/detail.html?id=${book.id}">${book.title}</a>`;
-        list.appendChild(div);
-      });
-    });
+async function showDetail(id) {
+  const res = await fetch(`/api/books/${id}`);
+  const b = await res.json();
+
+  alert(`📖 ${b.title}\n\n${b.content}`);
 }
 
-// 상세
-if (location.pathname.includes("detail.html")) {
-  const params = new URLSearchParams(location.search);
-  const id = params.get("id");
-
-  fetch(`${API}/books/${id}`)
-    .then((res) => res.json())
-    .then((book) => {
-      document.getElementById("title").innerText = book.title;
-      document.getElementById("content").innerText = book.content;
-    });
-}
+loadBooks();
